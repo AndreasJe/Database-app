67f959b7b3f23ecd96cfb23f918e4c07f9539dfd
AndreasJe <gud.er.gud@gmail.com>
2021-12-14 15:19:39 +0100

    Some changes to the values. Varchars and such

--- postgres setup/elearning.sql
+++ postgres setup/elearning.sql
@@ -5,11 +5,11 @@
 
 CREATE TABLE IF NOT EXISTS public.instructor
 (
-    first_name "char",
-    last_name "char",
-    email "char" NOT NULL,
-    password "char" NOT NULL,
-    introduction_brief "char",
+    first_name character varying(100),
+    last_name character varying(100),
+    email character varying(100) NOT NULL,
+    password character varying(255) NOT NULL,
+    introduction_brief character varying(250),
     verified boolean,
     registration_date date,
     published_courses integer,
@@ -30,17 +30,17 @@ ALTER TABLE IF EXISTS public.instructor
 
 CREATE TABLE IF NOT EXISTS public.course
 (
-    course_title "char",
-    course_brief "char",
+    course_title character varying(100),
+    course_brief character varying(250),
     instructor_id bigint,
     num_of_chapters integer,
+    id serial,
     CONSTRAINT course_pkey PRIMARY KEY (id),
     CONSTRAINT instructor1 FOREIGN KEY (instructor_id)
         REFERENCES public.instructor (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID
-    id serial,
 )
 
 TABLESPACE pg_default;
@@ -55,18 +55,18 @@ ALTER TABLE IF EXISTS public.course
 CREATE TABLE IF NOT EXISTS public.course_chapter
 (
     course_id bigint NOT NULL,
-    chapter_title "char",
+    chapter_title character varying(100),
     content_reading boolean,
     content_video boolean,
     content_assignment boolean,
     required_time date,
+    id serial,
     CONSTRAINT course_chapter_pkey PRIMARY KEY (id),
     CONSTRAINT fk_course_id FOREIGN KEY (course_id)
         REFERENCES public.course (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID
-    id serial,
 )
 
 TABLESPACE pg_default;
@@ -82,12 +82,12 @@ CREATE TABLE IF NOT EXISTS public.course_chapter_content
 (
     course_chapter_id bigint,
     mandatory boolean,
+    id serial,
     CONSTRAINT course_chapter_content_pkey PRIMARY KEY (id),
     CONSTRAINT pk_course_chapter_id FOREIGN KEY (course_chapter_id)
         REFERENCES public.course_chapter (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
-    id serial,
 )
 
 TABLESPACE pg_default;
@@ -103,14 +103,14 @@ CREATE TABLE IF NOT EXISTS public.enrollment
 (
     course_id bigint,
     student_id bigint,
-    enrollment_date "char",
+    enrollment_date date,
+    id serial,
     CONSTRAINT enrollment_pkey PRIMARY KEY (id),
     CONSTRAINT pk_course_id FOREIGN KEY (course_id)
         REFERENCES public.course (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID
-    id serial,
 )
 
 TABLESPACE pg_default;
@@ -127,8 +127,8 @@ CREATE TABLE IF NOT EXISTS public.feedback
     enrollment_id bigint,
     rating integer,
     feedback text COLLATE pg_catalog."default",
-    CONSTRAINT feedback_pkey PRIMARY KEY (id)
     id serial,
+    CONSTRAINT feedback_pkey PRIMARY KEY (id)
 )
 
 TABLESPACE pg_default;
@@ -140,7 +140,8 @@ ALTER TABLE IF EXISTS public.feedback
 
 -- DROP TABLE IF EXISTS public.students;
 
-CREATE TABLE public.students (
+CREATE TABLE IF NOT EXISTS public.students
+(
     first_name character varying(50),
     last_name character varying(50),
     email character varying(100) NOT NULL,
@@ -148,10 +149,9 @@ CREATE TABLE public.students (
     courses_enrolled integer,
     courses_complete integer,
     verified boolean,
-    registration_date date
-    id serial,
-);
-
+    registration_date date,
+    id serial
+)
 
 ALTER TABLE IF EXISTS public.students
     OWNER to postgres;
